eb7136732df4c9b039db4a4429bd7f4ac55b4b11
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-08-18 15:04:44 +0530

    Created next route files

---
+++ app/auth/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const authPage = () => {
+  return (
+    <div>
+      SignIn
+    </div>
+  )
+}
+
+export default authPage
---
+++ app/upload/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const uploadRoute = () => {
+  return (
+    <div>
+      RESUME UPLOAD
+    </div>
+  )
+}
+
+export default uploadRoute
