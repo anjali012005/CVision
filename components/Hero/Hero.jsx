65a6f78b97b6b400f9a8899d72f1f1d69775e234
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-08-19 22:40:32 +0530

    Created interview cards

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,9 +1,265 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 const Hero = () => {
     return (
         <div>
-            Hero
+            <div className='flex justify-center'>
+                <Button
+                    variant="outline"
+                    className="hover:bg-[#c29e19]  text-[#c29e19] transition duration-300 ease-in-out transform hover:scale-105"
+                >Get Started</Button>
+            </div>
+
+            <h3 className='p-4'>Your Interviews Are Here :-</h3>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 gap-6'>
+                <Card className="w-full max-w-sm">
+                    <CardHeader>
+                        <CardTitle>Login to your account</CardTitle>
+                        <CardDescription>
+                            Enter your email below to login to your account
+                        </CardDescription>
+                        <CardAction>
+                            <Button variant="link">Sign Up</Button>
+                        </CardAction>
+                    </CardHeader>
+                    <CardContent>
+                        <form>
+                            <div className="flex flex-col gap-6">
+                                <div className="grid gap-2">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        placeholder="m@example.com"
+                                        required
+                                    />
+                                </div>
+                                <div className="grid gap-2">
+                                    <div className="flex items-center">
+                                        <Label htmlFor="password">Password</Label>
+                                        <a
+                                            href="#"
+                                            className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                        >
+                                            Forgot your password?
+                                        </a>
+                                    </div>
+                                    <Input id="password" type="password" required />
+                                </div>
+                            </div>
+                        </form>
+                    </CardContent>
+                    <CardFooter className="flex-col gap-2">
+                        <Button type="submit" className="w-full">
+                            Login
+                        </Button>
+                        <Button variant="outline" className="w-full">
+                            Login with Google
+                        </Button>
+                    </CardFooter>
+                </Card>
+
+                <Card className="w-full max-w-sm">
+                    <CardHeader>
+                        <CardTitle>Login to your account</CardTitle>
+                        <CardDescription>
+                            Enter your email below to login to your account
+                        </CardDescription>
+                        <CardAction>
+                            <Button variant="link">Sign Up</Button>
+                        </CardAction>
+                    </CardHeader>
+                    <CardContent>
+                        <form>
+                            <div className="flex flex-col gap-6">
+                                <div className="grid gap-2">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        placeholder="m@example.com"
+                                        required
+                                    />
+                                </div>
+                                <div className="grid gap-2">
+                                    <div className="flex items-center">
+                                        <Label htmlFor="password">Password</Label>
+                                        <a
+                                            href="#"
+                                            className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                        >
+                                            Forgot your password?
+                                        </a>
+                                    </div>
+                                    <Input id="password" type="password" required />
+                                </div>
+                            </div>
+                        </form>
+                    </CardContent>
+                    <CardFooter className="flex-col gap-2">
+                        <Button type="submit" className="w-full">
+                            Login
+                        </Button>
+                        <Button variant="outline" className="w-full">
+                            Login with Google
+                        </Button>
+                    </CardFooter>
+                </Card>
+
+                <Card className="w-full max-w-sm">
+                    <CardHeader>
+                        <CardTitle>Login to your account</CardTitle>
+                        <CardDescription>
+                            Enter your email below to login to your account
+                        </CardDescription>
+                        <CardAction>
+                            <Button variant="link">Sign Up</Button>
+                        </CardAction>
+                    </CardHeader>
+                    <CardContent>
+                        <form>
+                            <div className="flex flex-col gap-6">
+                                <div className="grid gap-2">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        placeholder="m@example.com"
+                                        required
+                                    />
+                                </div>
+                                <div className="grid gap-2">
+                                    <div className="flex items-center">
+                                        <Label htmlFor="password">Password</Label>
+                                        <a
+                                            href="#"
+                                            className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                        >
+                                            Forgot your password?
+                                        </a>
+                                    </div>
+                                    <Input id="password" type="password" required />
+                                </div>
+                            </div>
+                        </form>
+                    </CardContent>
+                    <CardFooter className="flex-col gap-2">
+                        <Button type="submit" className="w-full">
+                            Login
+                        </Button>
+                        <Button variant="outline" className="w-full">
+                            Login with Google
+                        </Button>
+                    </CardFooter>
+                </Card>
+
+                <Card className="w-full max-w-sm">
+                    <CardHeader>
+                        <CardTitle>Login to your account</CardTitle>
+                        <CardDescription>
+                            Enter your email below to login to your account
+                        </CardDescription>
+                        <CardAction>
+                            <Button variant="link">Sign Up</Button>
+                        </CardAction>
+                    </CardHeader>
+                    <CardContent>
+                        <form>
+                            <div className="flex flex-col gap-6">
+                                <div className="grid gap-2">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        placeholder="m@example.com"
+                                        required
+                                    />
+                                </div>
+                                <div className="grid gap-2">
+                                    <div className="flex items-center">
+                                        <Label htmlFor="password">Password</Label>
+                                        <a
+                                            href="#"
+                                            className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                        >
+                                            Forgot your password?
+                                        </a>
+                                    </div>
+                                    <Input id="password" type="password" required />
+                                </div>
+                            </div>
+                        </form>
+                    </CardContent>
+                    <CardFooter className="flex-col gap-2">
+                        <Button type="submit" className="w-full">
+                            Login
+                        </Button>
+                        <Button variant="outline" className="w-full">
+                            Login with Google
+                        </Button>
+                    </CardFooter>
+                </Card>
+
+                <Card className="w-full max-w-sm">
+                    <CardHeader>
+                        <CardTitle>Login to your account</CardTitle>
+                        <CardDescription>
+                            Enter your email below to login to your account
+                        </CardDescription>
+                        <CardAction>
+                            <Button variant="link">Sign Up</Button>
+                        </CardAction>
+                    </CardHeader>
+                    <CardContent>
+                        <form>
+                            <div className="flex flex-col gap-6">
+                                <div className="grid gap-2">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        placeholder="m@example.com"
+                                        required
+                                    />
+                                </div>
+                                <div className="grid gap-2">
+                                    <div className="flex items-center">
+                                        <Label htmlFor="password">Password</Label>
+                                        <a
+                                            href="#"
+                                            className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                        >
+                                            Forgot your password?
+                                        </a>
+                                    </div>
+                                    <Input id="password" type="password" required />
+                                </div>
+                            </div>
+                        </form>
+                    </CardContent>
+                    <CardFooter className="flex-col gap-2">
+                        <Button type="submit" className="w-full">
+                            Login
+                        </Button>
+                        <Button variant="outline" className="w-full">
+                            Login with Google
+                        </Button>
+                    </CardFooter>
+                </Card>
+            </div>
         </div>
     )
 }
