1479a080423c2eee87b26d8619552cc91e432ff8
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-08-20 17:06:43 +0530

    Created User Model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,23 @@
-const userSchema = new Schema.mongoose
+const mongoose = require('mongoose');
+const { minLength } = require('zod');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+}, {
+    timestamps: true
+})
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
