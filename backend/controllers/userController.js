5428848bc8149e07eab6add52e9acc06a97a2083
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-08-22 01:13:55 +0530

    Created Login Route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,22 +3,22 @@ const User = require("../models/User");
 
 
 // Sign Up / Reister User
-const registerUser = async(req, res)=>{
+const registerUser = async (req, res) => {
     try {
-        const {name, email, password} = req.body;
+        const { name, email, password } = req.body;
 
         // console.log({email});
 
         //All feilds are mendatory
-        if(!name || !email || !password){
+        if (!name || !email || !password) {
             res.status(500).json({
                 message: 'All feilds are mendatory!'
             })
         }
 
         //check if user already exists
-        const existingUser = User.findOne({email});
-        if(existingUser){
+        const existingUser = User.findOne({ email });
+        if (existingUser) {
             res.status(500).json({
                 message: 'User already exist!'
             })
@@ -33,7 +33,7 @@ const registerUser = async(req, res)=>{
 
         res.status(200).json({
             message: "User registered successfully!",
-            user:{
+            user: {
                 id: user._id,
                 name: user.name,
                 email: user.email
@@ -48,4 +48,37 @@ const registerUser = async(req, res)=>{
     }
 }
 
-module.exports = {registerUser};
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            res.status(500).json({
+                message: 'User Not Found. Please Register!'
+            })
+        }
+
+        if (password != user.password) {
+            res.status(500).json({
+                message: 'Password is wrong!'
+            })
+        }
+
+        res.status(200).json({
+            message: "Logged In!",
+            user: {
+                email,
+                password
+            }
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Login failed!"
+        })
+    }
+}
+
+module.exports = { registerUser, loginUser };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { registerUser } = require("../controllers/userController");
+const { registerUser, loginUser } = require("../controllers/userController");
 const express = require('express');
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 
 // Register route
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
