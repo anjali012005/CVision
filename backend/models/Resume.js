5c29d59d43ee4bea6ebdcb83b2a2815f0915e841
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-08-25 16:18:29 +0530

    Created Resume model

---
+++ backend/models/Resume.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const ResumeSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        resumeUrl: {
+            type: String,
+            required: true
+        },
+        uploadedAt: {
+            type: Date,
+            default: Date.now
+        }
+    });
+
+module.exports = mongoose.model("Resume", ResumeSchema);
